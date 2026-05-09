e4468f586e39a9dbbe653a7c16e1d6dbbaf67343
shaikhwassaf <shaikhwassaf@gmail.com>
2026-05-10 03:34:29 +0500

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Digital Lounge: Live</h1>
+      <p>The foundation is connected to Supabase!</p>
+    </div>
+  </React.StrictMode>,
+)
