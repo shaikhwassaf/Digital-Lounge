69d6260040f8c808593075affd611ca5096f9374
shaikhwassaf <shaikhwassaf@gmail.com>
2026-05-10 03:49:15 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App' // Import the new App component
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>Digital Lounge: Live</h1>
-      <p>The foundation is connected to Supabase!</p>
-    </div>
+    <App />
   </React.StrictMode>,
 )
